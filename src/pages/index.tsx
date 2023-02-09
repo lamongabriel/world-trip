d3c4e5be0bd6971376466f5f95a1c02bd367ddae
Gabriel Lamon <gabriel-lamon@outlook.com>
2023-02-08 21:58:01 -0300

    home images section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,11 @@ import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 
 import bg from '@/assets/BlueBg.png'
 import airplane from '@/assets/Airplane.svg'
+import nightLife from '@/assets/Nightlife.svg'
+import beach from '@/assets/Beach.svg'
+import modern from '@/assets/modern.svg'
+import classic from '@/assets/Classic.svg'
+import more from '@/assets/More.svg'
 
 export default function Home() {
   return (
@@ -19,10 +24,10 @@ export default function Home() {
         <Container h='full'>
           <Flex align='center' justify='space-between' h='full'>
             <Flex direction='column' gap='1.5rem'>
-              <Heading>
+              <Heading color='brand.lightHeadingText'>
                 5 Continents, <br/> infinite possibilities.
               </Heading>
-              <Text>
+              <Text color='brand.lightInfo'>
                 It&apos;s finally time to get off the drawing board that <br/> trip you&apos;ve been dreamming about.
               </Text>
             </Flex>
@@ -32,6 +37,50 @@ export default function Home() {
           </Flex>
         </Container>
       </Box>
+      <Container>
+        <Flex py={20} justifyContent='space-between'>
+          <Box textAlign='center' maxW={40} w='full'>
+            <Box w={20} mx='auto'>
+              <Image src={nightLife} alt='Night Life Drink'/>
+            </Box>
+            <Text mt={4} fontWeight={600} fontSize='2xl'>
+              night life
+            </Text>
+          </Box>
+          <Box textAlign='center' maxW={40} w='full'>
+            <Box w={20} mx='auto'>
+              <Image src={beach} alt='Night Life Drink'/>
+            </Box>
+            <Text mt={4} fontWeight={600} fontSize='2xl'>
+              beach
+            </Text>
+          </Box>
+          <Box textAlign='center' maxW={40} w='full'>
+            <Box w={20} mx='auto'>
+              <Image src={modern} alt='Night Life Drink'/>
+            </Box>
+            <Text mt={4} fontWeight={600} fontSize='2xl'>
+              modern
+            </Text>
+          </Box>
+          <Box textAlign='center' maxW={40} w='full'>
+            <Box w={20} mx='auto'>
+              <Image src={classic} alt='Night Life Drink'/>
+            </Box>
+            <Text mt={4} fontWeight={600} fontSize='2xl'>
+              classic
+            </Text>
+          </Box>
+          <Box textAlign='center' maxW={40} w='full'>
+            <Box w={20} mx='auto'>
+              <Image src={more} alt='Night Life Drink'/>
+            </Box>
+            <Text mt={4} fontWeight={600} fontSize='2xl'>
+              and more...
+            </Text>
+          </Box>
+        </Flex>
+      </Container>
     </Layout>
   )
 }
